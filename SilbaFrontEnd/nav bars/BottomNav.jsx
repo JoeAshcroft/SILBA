84efd42a0d54b532d3d61549d7db37219e1483fd
tashkenna <natashakenna@gmail.com>
2023-07-31 20:42:10 +0100

    home, map and profile icons added to bottom nav bar

--- SilbaFrontEnd/nav bars/BottomNav.jsx
+++ SilbaFrontEnd/nav bars/BottomNav.jsx
@@ -1,5 +1,6 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import {NavigationContainer} from '@react-navigation/native'
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 import MapScreen from '../screens/HomeScreen';
 import HomeScreen from '../screens/HomeScreen';
@@ -10,9 +11,28 @@ const Tab = createBottomTabNavigator()
 export const BottomNav = () => {
     return (<NavigationContainer>
     <Tab.Navigator>
-    <Tab.Screen name="Home" component={HomeScreen} />
-    <Tab.Screen name="Map" component={MapScreen} />
-    <Tab.Screen name="Profile" component={ProfileScreen} />
+    <Tab.Screen name="Home" component={HomeScreen} 
+    options={{
+            tabBarIcon: () => (
+              <Ionicons name='home' size={32} color='black' />
+            ),
+          }} />
+    <Tab.Screen name="Map" component={MapScreen} 
+      options={{
+            tabBarIcon: () => (
+              <Ionicons name='map' size={32} color='black' />
+            ),
+          }}
+    />
+    <Tab.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{
+            tabBarIcon: () => (
+              <Ionicons name='person' size={32} color='black' />
+            ),
+          }}
+        />
     </Tab.Navigator>
   </NavigationContainer>)
 }
