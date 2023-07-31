5a66f1d366e0e320b7e69074f99369768e9f55d0
tashkenna <natashakenna@gmail.com>
2023-07-31 16:01:12 +0100

    bottom nav bar complete and top menu in progress

--- SilbaFrontEnd/nav bars/TopNav.js
+++ SilbaFrontEnd/nav bars/TopNav.js
@@ -1,12 +1,15 @@
-import { Menu, Box, Icon } from 'native-base';
-import { Pressable, Text } from 'react-native';
-
+import { Menu, Box } from 'native-base';
+import { Pressable } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export const TopNav = () => {
-  return <Box w="90%" alignItems="center">
-      <Menu w="190" trigger={triggerProps => {
+  return <Box w="90%" alignItems="rught" offsetY={30}>
+      <Menu w="190" 
+        placement="left"
+        
+        trigger={triggerProps => {
       return <Pressable accessibilityLabel="More options menu" {...triggerProps}>
-             <Icon type="MaterialIcons" name="menu" size={30} color="black" />
+             <Ionicons name='menu' size={32} color='black' />
             </Pressable>;
     }}>
         <Menu.Item>Restaurants</Menu.Item>
