c052cbd42b415e607898ddac7cb97a1480d2b5b9
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-08-01 15:05:39 +0100

    add working mapview

--- SilbaFrontEnd/screens/MapScreen.js
+++ SilbaFrontEnd/screens/MapScreen.js
@@ -1,11 +1,21 @@
-import { View, Text} from 'react-native'
-import React from 'react'
-
+import { View, StyleSheet } from "react-native";
+import MapView from "react-native-maps";
+import React from "react";
 
 export default function MapScreen() {
-    return (
-        <View style={{flex:1,justifyContent:"center",alignItems:"center"}}>
-          <Text>Map Screen</Text>
-        </View>
-    )
+  return (
+    <View style={styles.container}>
+      <MapView style={styles.map} />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+});
