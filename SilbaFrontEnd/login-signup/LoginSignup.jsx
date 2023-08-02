29ff726b78e0c3a40452dea5108784fb47801e81
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-08-02 10:15:06 +0100

    add login-signup page

---
+++ SilbaFrontEnd/login-signup/LoginSignup.jsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export const LoginSignup = () => {
+  return (
+    <View>
+      <Text>loginsignup</Text>
+    </View>
+  );
+};
--- SilbaFrontEnd/screens/ProfileScreen.js
+++ SilbaFrontEnd/screens/ProfileScreen.js
@@ -1,10 +1,12 @@
 import { Text } from "react-native";
 import { View } from "react-native";
+import { LoginSignup } from "../login-signup/LoginSignup";
 
 export default function ProfileScreen() {
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
           <Text>Profile Screen</Text>
+          <LoginSignup/>
         </View>
       );
 }
