972cf252d159d166b0003f9362f47158d96fde38
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-08-02 11:38:23 +0100

    add login signup forms

--- SilbaFrontEnd/login-signup/LoginSignup.jsx
+++ SilbaFrontEnd/login-signup/LoginSignup.jsx
@@ -1,9 +1,78 @@
-import { View, Text } from "react-native";
+import { View, Text, TextInput, Button } from "react-native";
+import { Formik } from "formik";
+import { useState } from "react";
+
+const formStyles = () => {};
+
+const Login = () => (
+  <View>
+    <Text>Login</Text>
+    <Formik
+      initialValues={{ email: "", password: "" }}
+      onSubmit={(values) => console.log(values)}
+    >
+      {({ handleChange, handleBlur, handleSubmit, values }) => (
+        <View>
+          <TextInput
+            placeholder="email"
+            onChangeText={handleChange("email")}
+            onBlur={handleBlur("email")}
+            value={values.email}
+          />
+          <TextInput
+            placeholder="password"
+            onChangeText={handleChange("password")}
+            onBlur={handleBlur("password")}
+            value={values.password}
+          />
+          <Button onPress={handleSubmit} title="Submit" />
+        </View>
+      )}
+    </Formik>
+  </View>
+);
+
+const Signup = () => (
+  <View>
+    <Text>Sign-up</Text>
+    <Formik
+      initialValues={{ email: "", password: "", username: "" }}
+      onSubmit={(values) => console.log(values)}
+    >
+      {({ handleChange, handleBlur, handleSubmit, values }) => (
+        <View>
+          <TextInput
+            placeholder="username"
+            onChangeText={handleChange("username")}
+            onBlur={handleBlur("username")}
+            value={values.username}
+          />
+          <TextInput
+            placeholder="email"
+            onChangeText={handleChange("email")}
+            onBlur={handleBlur("email")}
+            value={values.email}
+          />
+          <TextInput
+            placeholder="password"
+            onChangeText={handleChange("password")}
+            onBlur={handleBlur("password")}
+            value={values.password}
+          />
+          <Button onPress={handleSubmit} title="Submit" />
+        </View>
+      )}
+    </Formik>
+  </View>
+);
 
 export const LoginSignup = () => {
+  const [login, setLogin] = useState(true);
+
   return (
     <View>
-      <Text>loginsignup</Text>
+      {login ? <Login /> : <Signup />}
+      <Button onPress={() => setLogin(!login)} title="switch" />
     </View>
   );
 };
