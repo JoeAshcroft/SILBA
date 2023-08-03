fe743136dbc68c34b33c0a259487cb92b911d64c
KeeseRex <joeashcroft@hotmail.co.uk>
2023-08-03 15:32:50 +0100

    slightly tidy up code

--- SilbaFrontEnd/screens/ProfileScreen.js
+++ SilbaFrontEnd/screens/ProfileScreen.js
@@ -7,7 +7,7 @@ const userData = [
     username: "foodlover123",
     email: "joebloggs@fakemail.com",
     avatar_url:
-      "https://images.unsplash.com/photo-1633957897986-70e83293f3ff?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=693&q=80",
+      "https://images.unsplash.com/photo-1533738363-b7f9aef128ce?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=735&q=80",
     user_id: 1,
   },
 ];
@@ -20,13 +20,12 @@ export default function ProfileScreen() {
           return (
             <View key={user.user_id}>
               <Center>
-                <Heading size="2xl">{user.username}</Heading>
+                <Heading size="xl">{user.username}</Heading>
                 <Avatar
                   bg="green.500"
                   size="2xl"
                   source={{ uri: user.avatar_url }}
                 ></Avatar>
-
                 <Text style={styles.profileText}>{user.fullName}</Text>
                 <Text style={styles.profileText}>{user.email}</Text>
               </Center>
