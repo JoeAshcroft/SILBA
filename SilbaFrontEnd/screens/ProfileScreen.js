b4c8dbe105dc08276296f0c498c482a7498fc131
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-08-03 15:17:09 +0100

    remove login/signup element from profilescreen

--- SilbaFrontEnd/screens/ProfileScreen.js
+++ SilbaFrontEnd/screens/ProfileScreen.js
@@ -6,7 +6,6 @@ export default function ProfileScreen() {
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
           <Text>Profile Screen</Text>
-          <LoginSignup/>
         </View>
       );
 }
