5b5eb1c06d4ae3d83e2cf10c09cd4b08be35cf9b
tashkenna <natashakenna@gmail.com>
2023-08-03 17:30:15 +0100

    business pages updated with reviews, directions and styling

--- SilbaFrontEnd/screens/homeStack/BusinessDetailsScree.js
+++ SilbaFrontEnd/screens/homeStack/BusinessDetailsScree.js
@@ -4,6 +4,7 @@ import { Text, View, StyleSheet, Image, ScrollView, TouchableOpacity, Linking } 
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useLayoutEffect } from 'react';
 import { Ionicons } from "@expo/vector-icons";
+import ReviewsList from '../../components/ReviewsList';
 
 
 export default function BusinessDetailsScreen() {
@@ -18,7 +19,7 @@ export default function BusinessDetailsScreen() {
   }, [])
 
   const handleOpenMaps = () => {
-    const mapsUrl = `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(business.address)}`;
+    const mapsUrl = `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(business.business_name+business.address)}`;
     Linking.openURL(mapsUrl).catch(err => console.error('Error opening maps:', err));
   };
 
@@ -32,9 +33,26 @@ export default function BusinessDetailsScreen() {
           </View>
           <Text style={styles.address}>{business.address}</Text>
           <TouchableOpacity style={styles.directionsButton} onPress={handleOpenMaps}>
-            <Ionicons name="navigate"></Ionicons>
+            <View style={styles.directionsContainer}>
+            <Ionicons name="navigate"></Ionicons> 
+            <Text>get directions</Text>
+            </View>
           </TouchableOpacity>
+          <Text style={styles.descriptionTitle}>about the {business.category}</Text>
           <Text style={styles.longDescription}>{business.long_description}</Text>
+
+          <View>
+            <Text style={styles.reviewsTitle}>reviews</Text>
+            <TouchableOpacity>
+            <Text style={styles.addReview}>add review</Text>
+          </TouchableOpacity>
+            
+            <ReviewsList business={business}/>
+          </View>
+
+          <View> 
+          
+          </View>
         </ScrollView>
       </View>
     </SafeAreaView>
@@ -55,13 +73,14 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
   image: {
-    width: 300,
+    width: 350,
     height: 200,
   },
   businessName: {
     fontSize: 24,
     fontWeight: 'bold',
     marginBottom: 10,
+    textAlign: 'center',
   },
   description: {
     fontSize: 18,
@@ -77,21 +96,45 @@ const styles = StyleSheet.create({
     fontSize: 14,
     marginBottom: 10,
     color: 'gray',
+    textAlign: "center"
   },
   businessContainer: {
     width: 350,
     alignItems: 'center',
   },
+  directionsContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
   directionsButton: {
-    backgroundColor: 'blue',
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-    borderRadius: 8,
-    marginTop: 10,
+    paddingBottom: 10,
   },
   directionsButtonText: {
     color: 'white',
     fontWeight: 'bold',
     textAlign: 'center',
   },
+  descriptionTitle: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    paddingBottom: 5,
+  },
+  reviewsContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  reviewsTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    paddingBottom: 5,
+    textAlign: 'center'
+  }, 
+  addReview: {
+    fontSize:15,
+    
+    paddingBottom: 5,
+    textAlign: 'center'
+  }
 });
