f0fcae710c7354b871abaa581056efbb9385bd05
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-08-03 15:15:04 +0100

    add basic login/signup error handling

--- SilbaFrontEnd/login-signup/LoginSignup.jsx
+++ SilbaFrontEnd/login-signup/LoginSignup.jsx
@@ -1,35 +1,46 @@
 import { View, Text, Button } from "react-native";
-import { NativeBaseProvider, Stack, Input, Icon, Pressable } from "native-base";
+import {
+  NativeBaseProvider,
+  Stack,
+  Input,
+  Icon,
+  Pressable,
+  VStack,
+  Alert,
+  Box,
+} from "native-base";
 import { MaterialIcons } from "@expo/vector-icons";
 import { Formik } from "formik";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const LoginSignup = () => {
   const [login, setLogin] = useState(true);
   const [show, setShow] = useState(false);
+  const [error, setError] = useState(false);
+  const [signupErr, setSignupErr] = useState(null);
 
   const handleLogin = (formObj) => {
-    const { email, password } = formObj
-    // userLoginAPiFunc(props)
-    // .then((data)=>{
-  
-    // })
-    // .catch((err)=>{
-      
-    // })
-  }
-  
+    const { email, password } = formObj;
+    //   useEffect(() => {
+    //   userLoginAPiFunc(props)
+    //     .then((data) => {})
+    //     .catch((err) => {
+    setError(true);
+    //   });
+    // },[])
+  };
+
   const handleSignup = (formObj) => {
-    const { username, email, password } = formObj
-    // userSignupAPiFunc(props)
-    // .then((data)=>{
-  
-    // })
-    // .catch((err)=>{
-      
-    // })
-  }
-  
+    const { username, email, password } = formObj;
+    // useEffect(() => {
+    //   userSingupAPiFunc(props)
+    //     .then((data) => {})
+    //     .catch((err) => {
+    setError(true);
+    // });
+    // },[])
+  };
+
   const Login = () => {
     return (
       <View style={{ width: "100%" }}>
@@ -90,7 +101,7 @@ export const LoginSignup = () => {
       </View>
     );
   };
-  
+
   const Signup = () => {
     return (
       <View style={{ width: "100%" }}>
@@ -100,7 +111,7 @@ export const LoginSignup = () => {
           onSubmit={(values) => handleSignup(values)}
         >
           {({ handleChange, handleBlur, handleSubmit, values }) => (
-              <Stack w="100%" space={4} alignItems="center">
+            <Stack w="100%" space={4} alignItems="center">
               <Input
                 w={{
                   base: "75%",
@@ -170,9 +181,27 @@ export const LoginSignup = () => {
     );
   };
 
+  const ErrorSnackbar = () => (
+    <Alert w="100%" status="warning">
+      <VStack space={1} flexShrink={1} w="100%" alignItems="center">
+        <Alert.Icon size="md" />
+        <Text
+          fontSize="md"
+          fontWeight="medium"
+          _dark={{
+            color: "coolGray.800",
+          }}
+        >
+          {login ? "Incorrect email or password!" : "User already exists!"}
+        </Text>
+      </VStack>
+    </Alert>
+  );
+
   return (
     <NativeBaseProvider>
-      {login ? <Login /> : <Signup />}
+      { error ? <ErrorSnackbar /> : null }
+      { login ? <Login /> : <Signup /> }
       <Button onPress={() => setLogin(!login)} title="switch" />
     </NativeBaseProvider>
   );
