12ae452d96024bd148b38ef10c80d81053d8cc69
KeeseRex <joeashcroft@hotmail.co.uk>
2023-08-03 14:39:55 +0100

    add basic profile info to profile page

--- SilbaFrontEnd/screens/ProfileScreen.js
+++ SilbaFrontEnd/screens/ProfileScreen.js
@@ -1,43 +1,45 @@
-import { Text, SafeAreaView, StyleSheet } from "react-native";
-import { Avatar, Center } from "native-base";
+import { Text, SafeAreaView, StyleSheet, Dimensions } from "react-native";
+import { Avatar, Center, View, Heading, VStack } from "native-base";
+
+const userData = [
+  {
+    fullName: "Joe Bloggs",
+    username: "foodlover123",
+    email: "joebloggs@fakemail.com",
+    avatar_url:
+      "https://images.unsplash.com/photo-1633957897986-70e83293f3ff?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=693&q=80",
+    user_id: 1,
+  },
+];
 
 export default function ProfileScreen() {
   return (
-    <SafeAreaView style={styles.topTitle}>
+    <SafeAreaView>
       <Center>
-        <Avatar
-          bg="green.500"
-          size="2xl"
-          source={{
-            uri: "https://images.unsplash.com/photo-1633957897986-70e83293f3ff?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=693&q=80",
-          }}
-        ></Avatar>
+        {userData.map((user) => {
+          return (
+            <View key={user.user_id}>
+              <Center>
+                <Heading size="2xl">{user.username}</Heading>
+                <Avatar
+                  bg="green.500"
+                  size="2xl"
+                  source={{ uri: user.avatar_url }}
+                ></Avatar>
+
+                <Text style={styles.profileText}>{user.fullName}</Text>
+                <Text style={styles.profileText}>{user.email}</Text>
+              </Center>
+            </View>
+          );
+        })}
       </Center>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingVertical: 10,
-  },
-  titleContainer: {
-    paddingHorizontal: 10,
-    marginTop: 0,
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  topTitle: {
-    fontSize: 20,
-    fontWeight: "regular",
-    textAlign: "center",
-    margin: 10,
-  },
-  inputContainer: {
-    justifyContent: "center",
-    alignItems: "center",
+  profileText: {
+    fontSize: 30,
   },
 });
