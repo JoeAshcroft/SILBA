ab5fa0f18e58850ee470410570823ec0fee6ac6c
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-08-03 14:18:54 +0100

    reformat login/signup page

--- SilbaFrontEnd/login-signup/LoginSignup.jsx
+++ SilbaFrontEnd/login-signup/LoginSignup.jsx
@@ -1,78 +1,160 @@
-import { View, Text, TextInput, Button } from "react-native";
+import { View, Text, Button } from "react-native";
+import { NativeBaseProvider, Stack, Input, Icon, Pressable } from "native-base";
+import { Entypo, MaterialIcons } from "@expo/vector-icons";
 import { Formik } from "formik";
 import { useState } from "react";
 
 const formStyles = () => {};
 
-const Login = () => (
-  <View>
-    <Text>Login</Text>
-    <Formik
-      initialValues={{ email: "", password: "" }}
-      onSubmit={(values) => console.log(values)}
-    >
-      {({ handleChange, handleBlur, handleSubmit, values }) => (
-        <View>
-          <TextInput
-            placeholder="email"
-            onChangeText={handleChange("email")}
-            onBlur={handleBlur("email")}
-            value={values.email}
-          />
-          <TextInput
-            placeholder="password"
-            onChangeText={handleChange("password")}
-            onBlur={handleBlur("password")}
-            value={values.password}
-          />
-          <Button onPress={handleSubmit} title="Submit" />
-        </View>
-      )}
-    </Formik>
-  </View>
-);
+const Login = () => {
+  const [show, setShow] = useState(false);
+  return (
+    <View style={{ width: "100%" }}>
+      <Text>Login</Text>
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        onSubmit={(values) => console.log(values)}
+      >
+        {({ handleChange, handleBlur, handleSubmit, values }) => (
+          <Stack w="100%" space={4} alignItems="center">
+            <Input
+              w={{
+                base: "75%",
+                md: "25%",
+              }}
+              InputLeftElement={
+                <Icon
+                  as={<MaterialIcons name="email" />}
+                  size={5}
+                  ml="2"
+                  color="muted.400"
+                />
+              }
+              placeholder="Email Address"
+              onChangeText={handleChange("email")}
+              onBlur={handleBlur("email")}
+              value={values.email}
+            />
+            <Input
+              w={{
+                base: "75%",
+                md: "25%",
+              }}
+              type={show ? "text" : "password"}
+              InputRightElement={
+                <Pressable onPress={() => setShow(!show)}>
+                  <Icon
+                    as={
+                      <MaterialIcons
+                        name={show ? "visibility" : "visibility-off"}
+                      />
+                    }
+                    size={5}
+                    mr="2"
+                    color="muted.400"
+                  />
+                </Pressable>
+              }
+              placeholder="Password"
+              onChangeText={handleChange("password")}
+              onBlur={handleBlur("password")}
+              value={values.password}
+            />
+            <Button onPress={handleSubmit} title="Submit" />
+          </Stack>
+        )}
+      </Formik>
+    </View>
+  );
+};
 
-const Signup = () => (
-  <View>
-    <Text>Sign-up</Text>
-    <Formik
-      initialValues={{ email: "", password: "", username: "" }}
-      onSubmit={(values) => console.log(values)}
-    >
-      {({ handleChange, handleBlur, handleSubmit, values }) => (
-        <View>
-          <TextInput
-            placeholder="username"
-            onChangeText={handleChange("username")}
-            onBlur={handleBlur("username")}
-            value={values.username}
-          />
-          <TextInput
-            placeholder="email"
-            onChangeText={handleChange("email")}
-            onBlur={handleBlur("email")}
-            value={values.email}
-          />
-          <TextInput
-            placeholder="password"
-            onChangeText={handleChange("password")}
-            onBlur={handleBlur("password")}
-            value={values.password}
-          />
-          <Button onPress={handleSubmit} title="Submit" />
-        </View>
-      )}
-    </Formik>
-  </View>
-);
+const Signup = () => {
+  const [show, setShow] = useState(false);
+  return (
+    <View style={{ width: "100%" }}>
+      <Text>Sign-up</Text>
+      <Formik
+        initialValues={{ email: "", password: "", username: "" }}
+        onSubmit={(values) => console.log(values)}
+      >
+        {({ handleChange, handleBlur, handleSubmit, values }) => (
+            <Stack w="100%" space={4} alignItems="center">
+            <Input
+              w={{
+                base: "75%",
+                md: "25%",
+              }}
+              InputLeftElement={
+                <Icon
+                  as={<MaterialIcons name="person" />}
+                  size={5}
+                  ml="2"
+                  color="muted.400"
+                />
+              }
+              placeholder="Username"
+              onChangeText={handleChange("username")}
+              onBlur={handleBlur("username")}
+              value={values.username}
+            />
+            <Input
+              w={{
+                base: "75%",
+                md: "25%",
+              }}
+              InputLeftElement={
+                <Icon
+                  as={<MaterialIcons name="email" />}
+                  size={5}
+                  ml="2"
+                  color="muted.400"
+                />
+              }
+              placeholder="Email Address"
+              onChangeText={handleChange("email")}
+              onBlur={handleBlur("email")}
+              value={values.email}
+            />
+            <Input
+              w={{
+                base: "75%",
+                md: "25%",
+              }}
+              type={show ? "text" : "password"}
+              InputRightElement={
+                <Pressable onPress={() => setShow(!show)}>
+                  <Icon
+                    as={
+                      <MaterialIcons
+                        name={show ? "visibility" : "visibility-off"}
+                      />
+                    }
+                    size={5}
+                    mr="2"
+                    color="muted.400"
+                  />
+                </Pressable>
+              }
+              placeholder="Password"
+              onChangeText={handleChange("password")}
+              onBlur={handleBlur("password")}
+              value={values.password}
+            />
+            <Button onPress={handleSubmit} title="Submit" />
+          </Stack>
+        )}
+      </Formik>
+    </View>
+  );
+};
 
 export const LoginSignup = () => {
   const [login, setLogin] = useState(true);
 
   return (
-    <View>
+    <NativeBaseProvider>
       {login ? <Login /> : <Signup />}
       <Button onPress={() => setLogin(!login)} title="switch" />
-    </View>
+    </NativeBaseProvider>
   );
 };
