225e8c344902ff9f39f47152e83e7c02d347c60b
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-08-04 12:22:33 +0100

    add business markers to mapview]

--- SilbaFrontEnd/screens/MapScreen.js
+++ SilbaFrontEnd/screens/MapScreen.js
@@ -1,11 +1,37 @@
 import { View, StyleSheet } from "react-native";
-import MapView from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
 import React from "react";
+import data from "../data/businesses.json";
 
 export default function MapScreen() {
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} />
+      <MapView style={styles.map}>
+        {data.restaurants.map((marker)=>(
+          <Marker
+          key={marker.business_id}
+          coordinate={marker.location}
+          title={marker.business_name}
+          pinColor={'red'}
+        />
+        ))}
+        {data.experiences.map((marker)=>(
+          <Marker
+          key={marker.business_id}
+          coordinate={marker.location}
+          title={marker.business_name}
+          pinColor={'green'}
+        />
+        ))}
+        {data.shops.map((marker)=>(
+          <Marker
+          key={marker.business_id}
+          coordinate={marker.location}
+          title={marker.business_name}
+          pinColor={'blue'}
+        />
+        ))}
+      </MapView>
     </View>
   );
 }
@@ -15,7 +41,7 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   map: {
-    width: '100%',
-    height: '100%',
+    width: "100%",
+    height: "100%",
   },
 });
