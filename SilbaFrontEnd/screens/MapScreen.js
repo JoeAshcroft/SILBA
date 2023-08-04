38d013e326309b69b47370d04b6e3ce99f92e377
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-08-04 15:47:38 +0100

    add initial geolocation of user and backup location when perms denied

--- SilbaFrontEnd/screens/MapScreen.js
+++ SilbaFrontEnd/screens/MapScreen.js
@@ -1,35 +1,66 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Image } from "react-native";
 import MapView, { Marker } from "react-native-maps";
-import React from "react";
+import * as Location from "expo-location";
+import React, { useEffect, useState } from "react";
 import data from "../data/businesses.json";
 
 export default function MapScreen() {
+  const [location, setLocation] = useState({latitude: 53.483959, longitude: -2.244644});
+
+  useEffect(() => {
+    const getPermission = async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        return;
+      }
+      let currentLocation = await Location.getCurrentPositionAsync({});
+      setLocation(currentLocation.coords);
+    };
+    getPermission();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <MapView style={styles.map}>
-        {data.restaurants.map((marker)=>(
+      <MapView
+        style={styles.map}
+        region={{
+          latitude: location.latitude,
+          longitude: location.longitude,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01
+        }}
+      >
+        <Marker
+            key={"user"}
+            coordinate={{latitude: location.latitude,
+              longitude: location.longitude}}
+          >
+            <Image source={require("../assets/userLocation.png")} style={{height: 35, width:35 }} />
+          </Marker>
+          
+        {data.restaurants.map((marker) => (
           <Marker
-          key={marker.business_id}
-          coordinate={marker.location}
-          title={marker.business_name}
-          pinColor={'red'}
-        />
+            key={marker.business_id}
+            coordinate={marker.location}
+            title={marker.business_name}
+            pinColor={"red"}
+          />
         ))}
-        {data.experiences.map((marker)=>(
+        {data.experiences.map((marker) => (
           <Marker
-          key={marker.business_id}
-          coordinate={marker.location}
-          title={marker.business_name}
-          pinColor={'green'}
-        />
+            key={marker.business_id}
+            coordinate={marker.location}
+            title={marker.business_name}
+            pinColor={"green"}
+          />
         ))}
-        {data.shops.map((marker)=>(
+        {data.shops.map((marker) => (
           <Marker
-          key={marker.business_id}
-          coordinate={marker.location}
-          title={marker.business_name}
-          pinColor={'blue'}
-        />
+            key={marker.business_id}
+            coordinate={marker.location}
+            title={marker.business_name}
+            pinColor={"blue"}
+          />
         ))}
       </MapView>
     </View>
