c3c5b9437d3e67a0c8d2571523e2973fb0a10bb5
KeeseRex <joeashcroft@hotmail.co.uk>
2023-08-07 11:34:09 +0100

    revert searchbar back from Paper and add search to Restaurants, Shops and Experience pages

--- SilbaFrontEnd/screens/ShopsScreen.js
+++ SilbaFrontEnd/screens/ShopsScreen.js
@@ -1,74 +1,78 @@
-import React from 'react';
-import { SafeAreaView, View, FlatList, StyleSheet, Text, ScrollView } from 'react-native';
-import BusinessCard from '../components/BusinessCard';
-import data from '../data/businesses.json'
-import { Center, Input } from 'native-base';
-
+import React from "react";
+import { SafeAreaView, View, StyleSheet, Text, ScrollView } from "react-native";
+import BusinessCard from "../components/BusinessCard";
+import data from "../data/businesses.json";
+import { Input } from "native-base";
+import { useState } from "react";
 
 export default function ShopsScreen() {
-    return (
-        <SafeAreaView style={styles.container}>
-        
-          <ScrollView>
-          
-    
-         <View style={styles.inputContainer}>
-         <Text style={styles.topTitle}>Shops Near You</Text>
-          <Input variant="rounded" w="80%" 
+  const shops = data.shops;
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filteredShops, setFilteredShops] = useState(shops);
+
+  const searchFilterFunction = (search) => {
+    setSearchQuery(search);
+    const filteredShops = shops.filter((item) =>
+      item.address.toLowerCase().includes(search.toLowerCase())
+    );
+    setFilteredShops(filteredShops);
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <View style={styles.inputContainer}>
+          <Text style={styles.topTitle}>Shops Near You</Text>
+          <Input
+            variant="rounded"
+            w="80%"
             placeholder="enter your location"
             textAlign="center"
             marginBottom={4}
-    
-            onChangeText={(text) => {
-             
-            }}
+            onChangeText={searchFilterFunction}
+            value={searchQuery}
           />
         </View>
-            {renderScrollableList(data.shops, 'Shops')}
-          </ScrollView>
-        </SafeAreaView>
-      );
+        {renderScrollableList(filteredShops, "Shops")}
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
 
-      
-    };
+const renderScrollableList = (businessList, title) => (
+  <View>
+    <View style={styles.titleContainer}>
+      <Text style={styles.title}>{title}</Text>
+    </View>
+    <ScrollView vertical showsVerticalScrollIndicator={false}>
+      {businessList.map((business) => (
+        <BusinessCard key={business.business_id} business={business} />
+      ))}
+    </ScrollView>
+  </View>
+);
 
-    const renderScrollableList = (businessList, title) => (
-      <View>
-        <View style={styles.titleContainer}>
-          <Text style={styles.title}>{title}</Text>
-        </View>
-        <ScrollView vertical showsVerticalScrollIndicator={false}>
-          {businessList.map((business) => (
-            <BusinessCard key={business.business_id} business={business} />
-          ))}
-        </ScrollView>
-      </View>
-    );
-    
-    const styles = StyleSheet.create({
-      container: {
-        flex: 1,
-        paddingVertical: 10,
-      },
-      titleContainer: {
-        paddingHorizontal: 10,
-        marginTop: 0,
-      },
-      title: {
-        fontSize: 20,
-        fontWeight: 'bold',
-      },
-      topTitle: {
-        fontSize: 20,
-        fontWeight: 'regular',
-        textAlign: 'center',
-        margin: 10,
-      },
-      inputContainer: {
-        justifyContent: 'center',
-        alignItems: 'center',
-    
-    
-      },
-     
-    });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingVertical: 10,
+  },
+  titleContainer: {
+    paddingHorizontal: 10,
+    marginTop: 0,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  topTitle: {
+    fontSize: 20,
+    fontWeight: "regular",
+    textAlign: "center",
+    margin: 10,
+  },
+  inputContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
