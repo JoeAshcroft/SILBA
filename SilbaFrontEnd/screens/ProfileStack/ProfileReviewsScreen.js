9d9654d27f5cdca3aa0f276fb7dcf522c6df16d6
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-08-08 14:35:54 +0100

    add user profile review screen

--- SilbaFrontEnd/screens/ProfileStack/ProfileReviewsScreen.js
+++ SilbaFrontEnd/screens/ProfileStack/ProfileReviewsScreen.js
@@ -1,7 +1,21 @@
-import { Text } from "react-native"
+import { useRoute } from "@react-navigation/native";
+import { View } from "react-native";
+import ReviewCard from "../../components/ReviewCard.jsx"
+import reviewData from "../../data/reviews.json";
 
 export default ProfileReviewsScreen = () => {
-    return (
-        <Text>Reviews</Text>
-    )
-}
+  const route = useRoute();
+  const { user } = route.params
+
+  const reviews = reviewData.reviews.filter((review)=> {
+    return review.username === user.username
+  })
+
+  return (
+    <View>
+      {reviews.map((review) => (
+        <ReviewCard key={review.review_id} review={review} />
+      ))}
+    </View>
+  );
+};
