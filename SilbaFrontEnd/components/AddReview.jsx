cec01743b8c04a513f5591c547a1b291d3014719
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-08-09 16:50:43 +0100

    create review submission form and submit function

--- SilbaFrontEnd/components/AddReview.jsx
+++ SilbaFrontEnd/components/AddReview.jsx
@@ -1,54 +1,92 @@
-import React from "react";
-import { Text, TextInput, TouchableOpacity, View } from "react-native";
-import { StyleSheet } from "react-native";
+import React, { useState } from "react";
+import {
+  Text,
+  TextInput,
+  View,
+  StyleSheet,
+} from "react-native";
+import { Formik } from "formik";
+import { Button, Stack, Alert } from "native-base";
+import { Ionicons } from "@expo/vector-icons";
+import { newDate } from "../utils/utils";
+import { postReviews } from "../api/api"
 
-export default AddReview = () => {
-    return (
-        <View style={styles.container}>
+export default AddReview = ({businessID}) => {
+    const [error, setError] = useState(false)
+
+    const handleReviewPost = ({username, reviewBody, businessID}) => {
+        const reviewData = { review: reviewBody, business: businessID, user: username }
+        postReviews(reviewData)
+        .then((res)=>{
+            setError(false)
+            console.log(res.data)
+        })
+        .catch((err)=>{
+            console.log(err)
+            setError(true)
+        })
+    }
+
+  return (
+    <View style={styles.container}>
+        
+      <Formik
+        initialValues={{
+          username: "johndoe",
+          reviewBody: "",
+          userID: "64d39ab4ab5abbdd3d9fcead",
+          businessID: businessID,
+        }}
+        onSubmit={(reviewObj) => handleReviewPost(reviewObj)}
+      >
+        {({ handleChange, handleBlur, handleSubmit, values }) => (
+          <Stack w="100%" space={4} alignItems="center">
+            {error ? <Alert w="100%" title={"error"} status={"error"}>There was an error posting your comment!</Alert> : null}
             <TextInput
-                mode="outlined"
-                placeholder="Leave a review"
-                multiline 
-                numberOfLines={5} 
-                style={styles.textInput}
-                underlineColorAndroid="transparent" 
-                selectionColor="#000" 
+              mode="outlined"
+              placeholder="Leave a review"
+              multiline
+              numberOfLines={5}
+              style={styles.textInput}
+              underlineColorAndroid="transparent"
+              selectionColor="#000"
+              onChangeText={handleChange("reviewBody")}
+              onBlur={handleBlur("reviewBody")}
+              value={values.reviewBody}
             />
-            <TouchableOpacity style={styles.submitButton}>
-                <Text style={styles.submitText}>Submit</Text>
-            </TouchableOpacity>
-        </View>
-    );
+            <Button
+              title="Submit"
+              onPress={handleSubmit}
+              bgColor={"coolGray.300"}
+              p={"1"}
+              endIcon={<Ionicons name="send-outline" size={24} color="black" />}
+            >
+              <Text color={"black"}>Submit</Text>
+            </Button>
+          </Stack>
+        )}
+      </Formik>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flexDirection: "row",
-        alignItems: "center",
-        padding: 5,
-    },
-    textInput: {
-        backgroundColor: "#EDEDED", // Grey background
-        borderRadius: 100,
-        height: 40,
-        flex: 1, // Take up available space
-        paddingHorizontal: 20, // Horizontal padding
-        paddingVertical: 10, // Vertical padding
-        marginBottom: 10,
-        marginTop: 10,
-        marginRight: 10, // Add some spacing between TextInput and Button
-    },
-    submitButton: {
-        
-        borderRadius: 100,
-        alignItems: "center", // Center horizontally
-        justifyContent: "center", // Center vertically
-        paddingVertical: 10, // Vertical padding
-        width: 60,
-        height: 40,
-    },
-    submitText: {
-        fontSize: 12,
-    },
+  container: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 5,
+    width: "100%",
+  },
+  textInput: {
+    backgroundColor: "#EDEDED", // Grey background
+    borderRadius: 100,
+    height: 40,
+    width: "80%",
+    // flex: 1, // Take up available space
+    paddingHorizontal: 20, // Horizontal padding
+    paddingVertical: 10, // Vertical padding
+    marginBottom: 10,
+    marginTop: 10,
+    marginRight: 10, // Add some spacing between TextInput and Button
+  },
 });
-
