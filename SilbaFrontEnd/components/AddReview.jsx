ebe25f10376172e30c877a466d6ddbf4f83392af
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-08-10 10:21:17 +0100

    add post review to api with UX and optimistic rendering

--- SilbaFrontEnd/components/AddReview.jsx
+++ SilbaFrontEnd/components/AddReview.jsx
@@ -1,75 +1,94 @@
 import React, { useState } from "react";
-import {
-  Text,
-  TextInput,
-  View,
-  StyleSheet,
-} from "react-native";
+import { Text, TextInput, View, StyleSheet } from "react-native";
 import { Formik } from "formik";
 import { Button, Stack, Alert } from "native-base";
 import { Ionicons } from "@expo/vector-icons";
-import { postReviews } from "../api/api"
+import { postReviews } from "../api/api";
+import ReviewCard from "./ReviewCard";
 
-export default AddReview = ({businessID}) => {
-    const [error, setError] = useState(false)
-    const userID = "64d39ab4ab5abbdd3d9fcead"
+export default AddReview = ({ businessID }) => {
+  const [status, setStatus] = useState(false);
+  const [optimistic, setOptimistic] = useState(false);
+  const [optimisticData, setOptimisticData] = useState({});
+  const userID = "64d39ab4ab5abbdd3d9fcead";
 
-    const handleReviewPost = ({username, reviewBody, businessID}) => {
-        const reviewData = { review: reviewBody, businessId: businessID, username: username, userId: userID }
-        postReviews(reviewData)
-        .then((res)=>{
-            setError(false)
-        })
-        .catch((err)=>{
-            setError(true)
-        })
-    }
+  const handleReviewPost = ({ username, reviewBody, businessID }) => {
+    const reviewData = {
+      review: reviewBody,
+      businessId: businessID,
+      username: username,
+      userId: userID,
+      review_votes: 0,
+    };
+    setOptimisticData(reviewData);
+    setOptimistic(true);
+    postReviews(reviewData)
+      .then((res) => {
+        setStatus("success");
+      })
+      .catch((err) => {
+        setStatus("error");
+      });
+  };
 
   return (
     <View style={styles.container}>
-        
-      <Formik
-        initialValues={{
-          username: "johndoe",
-          reviewBody: "",
-          businessID: businessID,
-        }}
-        onSubmit={(reviewObj) => handleReviewPost(reviewObj)}
-      >
-        {({ handleChange, handleBlur, handleSubmit, values }) => (
-          <Stack w="100%" space={4} alignItems="center">
-            {error ? <Alert w="100%" title={"error"} status={"error"}>There was an error posting your comment!</Alert> : null}
-            <TextInput
-              mode="outlined"
-              placeholder="Leave a review"
-              multiline
-              numberOfLines={5}
-              style={styles.textInput}
-              underlineColorAndroid="transparent"
-              selectionColor="#000"
-              onChangeText={handleChange("reviewBody")}
-              onBlur={handleBlur("reviewBody")}
-              value={values.reviewBody}
-            />
-            <Button
-              title="Submit"
-              onPress={handleSubmit}
-              bgColor={"coolGray.300"}
-              p={"1"}
-              endIcon={<Ionicons name="send-outline" size={24} color="black" />}
-            >
-              <Text color={"black"}>Submit</Text>
-            </Button>
-          </Stack>
-        )}
-      </Formik>
+      {status === "error" ? (
+        <Alert w="100%" title={"error"} status={"error"}>
+          There was an error posting your review!
+        </Alert>
+      ) : status === "success" ? (
+        <Alert w="100%" title={"success"} status={"success"}>
+          Your review was posted successfully!
+        </Alert>
+      ) : null}
+      {optimistic ? (
+        <ReviewCard review={optimisticData} />
+      ) : (
+        <Formik
+          initialValues={{
+            username: "johndoe",
+            reviewBody: "",
+            businessID: businessID,
+          }}
+          onSubmit={(reviewObj) => handleReviewPost(reviewObj)}
+        >
+          {({ handleChange, handleBlur, handleSubmit, values }) => (
+            <Stack w="100%" space={4} alignItems="center">
+              <TextInput
+                mode="outlined"
+                placeholder="Leave a review"
+                multiline
+                numberOfLines={5}
+                style={styles.textInput}
+                underlineColorAndroid="transparent"
+                selectionColor="#000"
+                onChangeText={handleChange("reviewBody")}
+                onBlur={handleBlur("reviewBody")}
+                value={values.reviewBody}
+              />
+              <Button
+                title="Submit"
+                onPress={handleSubmit}
+                bgColor={"coolGray.300"}
+                p={"1"}
+                endIcon={
+                  <Ionicons name="send-outline" size={24} color="black" />
+                }
+              >
+                <Text color={"black"}>Submit</Text>
+              </Button>
+            </Stack>
+          )}
+        </Formik>
+      )}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: "row",
+    flexDirection: "column",
     alignItems: "center",
     padding: 5,
     width: "100%",
@@ -78,7 +97,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#EDEDED",
     borderRadius: 100,
     height: 40,
-    width: "80%",
+    width: 300,
     paddingHorizontal: 20,
     paddingVertical: 10,
     marginBottom: 10,
